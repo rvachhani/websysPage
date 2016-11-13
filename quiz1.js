50b5f13d8c85b49727a201c9d757d4903f386f4d
rvachhani <roshni.vachhani96@gmail.com>
2016-11-13 17:40:09 -0500

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,22 @@
+// The JavaScrip function that creates and AJAX get request in order to fill
+// The html div with the content of the xml file
+
+$(document).ready(function() {
+	$.ajax({
+		url: "resources/labs.xml",
+		dataType: "xml",
+		type: "GET",
+		success: function(myXml) {
+			var content = "<tr><th>Lab</th><th>Description</th></tr>";
+			$(myXml).find("entry").each(function() {
+				content += "<tr><td><a href='" + 
+				$(this).find("location").text() + "'>" + 
+				$(this).find("folderName").text() + "</a></td><td>" + 
+				$(this).find("description").text() + "</td></tr>";
+			});
+			document.getElementById("content").innerHTML = content;
+		}, err: function(msg) {
+              alert("There was a problem loading the xml file");
+        }
+	});
+});
