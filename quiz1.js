779ef8c8b788cef351aab5734e7953874a8be786
rvachhani <roshni.vachhani96@gmail.com>
2016-11-14 16:18:12 -0500

    Add files via upload

--- quiz1.js
+++ quiz1.js
@@ -1,4 +1,4 @@
-// The JavaScrip function that creates and AJAX get request in order to fill
+// The JavaScript function that creates and AJAX get request in order to fill
 // The html div with the content of the xml file
 
 $(document).ready(function() {
@@ -14,9 +14,29 @@ $(document).ready(function() {
 				$(this).find("folderName").text() + "</a></td><td>" + 
 				$(this).find("description").text() + "</td></tr>";
 			});
-			document.getElementById("content").innerHTML = content;
+			document.getElementById("lab_content").innerHTML = content;
 		}, err: function(msg) {
               alert("There was a problem loading the xml file");
         }
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url: "resources/hws.xml",
+		dataType: "xml",
+		type: "GET",
+		success: function(hws) {
+			var content = "<tr><th>Lab</th><th>Description</th></tr>";
+			$(hws).find("entry").each(function() {
+				content += "<tr><td><a href='" + 
+				$(this).find("location").text() + "'>" + 
+				$(this).find("folderName").text() + "</a></td><td>" + 
+				$(this).find("description").text() + "</td></tr>";
+			});
+			document.getElementById("hw_content").innerHTML = content;
+		}, err: function(msg) {
+			alert("There was a problem loading the xml file");
+		}
 	});
 });
